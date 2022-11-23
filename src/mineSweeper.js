14ebeeca48e981f9e60dbc4996a856f32f2ef990
Hayashizaki Masayuki / 林崎優之 <akaritakahashioss@gmail.com>
2022-11-23 16:16:20 +0900

    長押し時間を短くした🐱

--- src/mineSweeper.js
+++ src/mineSweeper.js
@@ -494,7 +494,7 @@ const Cell = ({ cell, handleCellLeftClick, handleCellRightClick, handleCellDoubl
         };
     };
 
-    const longPress = useLongPress((e) => handleCellRightClick(cell), 600);
+    const longPress = useLongPress((e) => handleCellRightClick(cell), 300);
     return (
         <div
             className={cellClassName}
